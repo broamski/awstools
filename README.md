a1cca27c7e5a1adc0ad329340ec061c9c99999ed
broamski <brian.nuszkowski@me.com>
2016-08-02 14:39:06 -0400

    Expand tag searching to be able to search on only key, only value or both

--- README.md
+++ README.md
@@ -7,6 +7,14 @@
 ## Included Tools
 ```
 metaq.py - Query instances by tag data. Returns: instance id, Name tag, private and public IP addresses
+    # Query for instances with a certain key
+    python metaq.py --query key --key <value_of_key>
+
+    # Query for instances with a certain value
+    python metaq.py --query value --value <value_of_value>
+
+    # Query for instances with a specified key/value combination
+    python metaq.py --query both --key <some_key> --value <some_value>
 ecs\
     task_def_diff.py - Perform a visual diff of Elastic Container Service Task Definitions
 cloudwatch\
--- utils/metaq.py
+++ utils/metaq.py
@@ -1,32 +1,73 @@
 import argparse
 import boto3
+import sys
 
 """
-Query instances based upon tag data"
+Query for instances based upon tag data"
 """
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-tn', '--tag-name', required=True)
-parser.add_argument('-tv', '--tag-value', required=True)
+parser.add_argument('-q', '--query-type', required=True,
+                    choices=["key", "value", "both"])
+parser.add_argument('-k', '--key')
+parser.add_argument('-v', '--value')
 parser.add_argument('--region', default='us-east-1')
 args = parser.parse_args()
+
+
+if args.query_type == "key":
+    if args.key is None:
+        sys.exit("Must provide --key when query type is 'key'")
+    filter = {
+        'Name': "tag-key",
+        'Values': [
+            args.key
+        ]
+    }
+
+if args.query_type == "value":
+    if args.value is None:
+        sys.exit("Must provide --value when query type is 'value'")
+    filter = {
+        'Name': "tag-value",
+        'Values': [
+            args.value
+        ]
+    }
+
+if args.query_type == "both":
+    if args.key is None:
+        sys.exit("Must provide --key")
+
+    if args.value is None:
+        sys.exit("Must provide --value")
+
+    filter = {
+        'Name': "tag:%s" % (args.key,),
+        'Values': [
+            args.value
+        ]
+    }
+
 
 client = boto3.client('ec2', region_name=args.region)
 
 instances = client.describe_instances(
     Filters=[
-        {
-            'Name': "tag:%s" % args.tag_name,
-            'Values': [
-                args.tag_value
-            ]
-        }]
+        filter
+    ]
 )
 
 for reservation in instances['Reservations']:
     for instance in reservation['Instances']:
         print "InstanceId: %s" % (instance['InstanceId'],)
-        print "Private IP: %s" % (instance['PrivateIpAddress'],)
+        public_ip = instance.get('PublicIpAddress')
+        if public_ip is not None:
+            print "Public IP: %s" % (public_ip,)
+        private_ip = instance.get('PrivateIpAddress')
+        if private_ip is not None:
+            print "Private IP: %s" % (private_ip,)
         tags = [tag for tag in instance['Tags'] if tag["Key"] == "Name"]
-        print "Name Tag: %s" % (tags[0]['Value'],)
+        if len(tags) > 0:
+            print "Name Tag: %s" % (tags[0]['Value'],)
         print "--------------------------"
